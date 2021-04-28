2db62b70aeada86df5bfedbc8dac254099a3d3e3
Duane J. May <duane.may@cengage.com>
2021-04-28 14:36:02 -0400

    Fix cloudformation update-stack command

--- deploy.sh
+++ deploy.sh
@@ -13,6 +13,6 @@ aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAM
 aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
 
 TEMPLATE_URL=https://${DIST_OUTPUT_BUCKET}-${REGION}.s3.amazonaws.com/${DIST_OUTPUT_BUCKET}/${VERSION}/serverless-image-handler.template
-echo deploy with URL: ${TEMPLATE_URL}
+echo TEMPLATE_URL=${TEMPLATE_URL}
 
-aws cloudformation update-stack --stack-name  ${CLOUDFORMATION_STACK} --template-url ${TEMPLATE_URL} --parameters "ParameterKey=SourceBuckets,UsePreviousValue=true"
+aws cloudformation update-stack --stack-name  ${CLOUDFORMATION_STACK} --template-url ${TEMPLATE_URL} --capabilities CAPABILITY_NAMED_IAM --parameters "ParameterKey=SourceBuckets,UsePreviousValue=true"
--- env.sh
+++ env.sh
@@ -3,7 +3,8 @@
 export REGION=us-east-1
 export DIST_OUTPUT_BUCKET=callisto-sih-test
 export SOLUTION_NAME=callisto-sih-test
-export VERSION=5.2.0-1
 
 export CLOUDFORMATION_STACK=callisto-sih
 export SOURCE_BUCKETS=callisto-sih-source1
+
+export VERSION=5.2.0-2
