49149ba1cfb99f6b7bab5f22fb70ffad756a3fde
Duane J. May <duane.may@cengage.com>
2021-04-28 14:14:50 -0400

    initial Deploy script

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+SCRIPT_PATH=$(cd "$(dirname "$0")"; pwd)
+# shellcheck source=env.sh
+. "$SCRIPT_PATH"/env.sh
+
+cd "$SCRIPT_PATH"/deployment
+
+./build-s3-dist.sh $DIST_OUTPUT_BUCKET $SOLUTION_NAME $VERSION
+
+aws s3 sync ./regional-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
+aws s3 sync ./global-s3-assets/ s3://$DIST_OUTPUT_BUCKET-$REGION/$SOLUTION_NAME/$VERSION/ --acl bucket-owner-full-control
+
+TEMPLATE_URL=https://${DIST_OUTPUT_BUCKET}-${REGION}.s3.amazonaws.com/${DIST_OUTPUT_BUCKET}/${VERSION}/serverless-image-handler.template
+echo deploy with URL: ${TEMPLATE_URL}
+
+aws cloudformation update-stack --stack-name  ${CLOUDFORMATION_STACK} --template-url ${TEMPLATE_URL} --parameters "ParameterKey=SourceBuckets,UsePreviousValue=true"
---
+++ env.sh
@@ -0,0 +1,9 @@
+#!/bin/false
+
+export REGION=us-east-1
+export DIST_OUTPUT_BUCKET=callisto-sih-test
+export SOLUTION_NAME=callisto-sih-test
+export VERSION=5.2.0-1
+
+export CLOUDFORMATION_STACK=callisto-sih
+export SOURCE_BUCKETS=callisto-sih-source1
